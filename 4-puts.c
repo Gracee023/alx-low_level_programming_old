63e19bcb09f7b4af435307d4e50327acd827a371
Gracee023 <gracekevwe@gmail.com>
2023-03-19 09:43:21 -0700

    Hello world

--- 4-puts.c
+++ 4-puts.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 /**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-              Put (“\”programming is like building a multilingual puzzle
-”);
-Return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
